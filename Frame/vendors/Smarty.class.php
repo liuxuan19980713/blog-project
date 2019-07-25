e45c79eee3cfb42df467cd2e0521a9da7abefc9f
liuxuan“;git config --global user.name liuxuan“ <1395380336@qq.com>
2019-07-25 12:31:30 +0800

    提交了一次

--- Frame/vendors/Smarty.class.php
+++ Frame/vendors/Smarty.class.php
@@ -1,5 +1,8 @@
 <?php  
  namespace Frame\Vendors;
+ /**
+  * 这是封装的Smarty类
+  */
 require_once(ROOT_PATH."Frame".DS."vendors".DS.    "smarty-libs/Smarty.class.php");
 class Smarty extends \Smarty{
 
